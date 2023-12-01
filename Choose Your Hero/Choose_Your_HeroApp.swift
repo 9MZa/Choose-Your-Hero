4fd9bd54d721e1fd9f4f8815671e1d8198bfc154
Bandit Silachai <mbandit001@yahoo.co.th>
2023-12-01 11:12:58 +0700

    Initial Commit

---
+++ Choose Your Hero/Choose_Your_HeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Choose_Your_HeroApp.swift
+//  Choose Your Hero
+//
+//  Created by Bandit Silachai on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Choose_Your_HeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
